284c02385673dac9715a09aacb80cb7302185b37
juanpythonvolei <juanzsalca@outlook.com>
2025-03-23 23:17:56 -0300

    'Ajustes_testes3'

--- functions/users/Login.py
+++ functions/users/Login.py
@@ -2,6 +2,8 @@ from database.engine.database import *
 def login(email:str,senha:int):
    try:
       session.query(Usuario).filter(Usuario.email == email,Usuario.senha == senha)
+      session.close()
       return True
    except:
+      session.rollback()
       return False
