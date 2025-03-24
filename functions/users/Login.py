7127a6c5de450cc934785378b79af10054ece520
juanpythonvolei <juanzsalca@outlook.com>
2025-03-23 23:25:20 -0300

    'login_conserto'

--- functions/users/Login.py
+++ functions/users/Login.py
@@ -1,9 +1,9 @@
 from database.engine.database import *
 def login(email:str,senha:int):
-
-   if session.query(Usuario).filter(Usuario.email == email,Usuario.senha == senha).first():
+   try:
+      session.query(Usuario).filter(Usuario.email == email,Usuario.senha == senha)
       session.close()
       return True
-   else:
+   except:
       session.rollback()
       return False
