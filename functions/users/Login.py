5d64efad048fbc9e1637df60ca7ff353c46383d9
juanpythonvolei <juanzsalca@outlook.com>
2025-03-23 23:14:53 -0300

    'Ajustes_testes2'

--- functions/users/Login.py
+++ functions/users/Login.py
@@ -1,12 +1,5 @@
 from database.engine.database import *
 def login(email:str,senha:int):
-  meu_usuario = st.secrets['meuusuario']
-  minha_senha = st.secrets['minhasenha']
-#   meu_usuario = os.getenv('meuusuario')
-#   minha_senha = os.getenv('minhasenha')
-  if email == str(meu_usuario) and senha == (minha_senha):
-     return True
-  else:
    try:
       session.query(Usuario).filter(Usuario.email == email,Usuario.senha == senha)
       return True
